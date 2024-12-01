ba27b29e70afe2a648b6f127ac55568fe528993a
Shreenidhi-br <164990814+Shreenidhi-br@users.noreply.github.com>
2024-12-01 22:36:22 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="grid">
+
+   <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:rachanarvnd123@gmail.com">rachanarvnd123@gmail.com</a>
+         <a href="mailto:radhuankolekar03@gmail.com">radhuankolekar03@gmail.com</a>
+         <a href="mailto:shreenidhi123@gmail.com">shreenidhi123@gmail.com</a>
+         <a href="mailto:vaishnavisavant03@gmail.com">vaishnavisavant03@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">VCET puttur</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:9108058153">9108058153</a>
+         <a href="tel:7975543849">7975543849</a>
+         <a href="tel:9496309237">9496309237</a>
+         <a href="tel:9353332711">9353332711</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Rachana,Radhika,Shreenidhi,Vaishnavi</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loa.gif" alt="">
+</div>
